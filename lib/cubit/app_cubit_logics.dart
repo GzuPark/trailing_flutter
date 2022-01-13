0d337055d688d095249ca5e289345eccce2736d8
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-13 15:59:30 +0900

    Apply to action back to the main page

--- lib/cubit/app_cubit_logics.dart
+++ lib/cubit/app_cubit_logics.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trailing_flutter/cubit/app_cubit_states.dart';
 import 'package:trailing_flutter/pages/detail_page.dart';
-import 'package:trailing_flutter/pages/home_page.dart';
+import 'package:trailing_flutter/pages/navpages/main_page.dart';
 import 'package:trailing_flutter/pages/welcome_page.dart';
 
 import 'app_cubits.dart';
@@ -27,7 +27,7 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
             return const Center(child: CircularProgressIndicator());
           }
           if (state is LoadedState) {
-            return const HomePage();
+            return const MainPage();
           }
           if (state is DetailState) {
             return const DetailPage();
